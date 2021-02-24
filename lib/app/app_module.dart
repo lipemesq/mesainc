83b5c707c8ff8569b166a92a623a3d11d92bc7fa
Felipe Mesquita <lipemesq@gmail.com>
2021-02-24 18:33:34 -0300

    cleaned initial stuff

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,19 +1,13 @@
-import 'app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
 import 'package:ps_mesainc/app/app_widget.dart';
-import 'package:ps_mesainc/app/modules/home/home_module.dart';
 
 class AppModule extends MainModule {
   @override
-  List<Bind> get binds => [
-        $AppController,
-      ];
+  List<Bind> get binds => [];
 
   @override
-  List<ModularRouter> get routers => [
-        ModularRouter(Modular.initialRoute, module: HomeModule()),
-      ];
+  List<ModularRouter> get routers => [];
 
   @override
   Widget get bootstrap => AppWidget();
